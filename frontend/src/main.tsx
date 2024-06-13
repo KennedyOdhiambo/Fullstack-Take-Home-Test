57270a69d6b8f5130eb637a9b12681e0b41601a3
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-06-13 18:15:46 +0300

    changes before merging main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,17 @@ import "./index.css"
 import "@fontsource/mulish"
 import "@fontsource/mulish/400.css"
 import "@fontsource/mulish/400-italic.css"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
